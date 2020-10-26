2cc9346299573c42c55c0ac65b8b0a6528fd69c0
AdrianHsu <adrianhsu1995@gmail.com>
2020-10-25 17:27:38 -0700

    [UPDATED] Sun Oct 25 17:27:37 PDT 2020 :bulb:

--- trainer/task.py
+++ trainer/task.py
@@ -13,12 +13,148 @@ def load(data_dir):
 
   return raw_dataset
 
-def run(work_dir, train_max_epoches):
+def build_rating_sparse_tensor(ratings_df):
+  """
+  Args:
+    ratings_df: a pd.DataFrame with `user_id`, `movie_id` and `rating` columns.
+  Returns:
+    a tf.SparseTensor representing the ratings matrix.
+  """
+  indices = ratings_df[['user_id', 'movie_id']].values
+  values = ratings_df['rating'].values
+  return tf.SparseTensor(
+      indices=indices,
+      values=values,
+      dense_shape=[users.shape[0], movies.shape[0]])
+
+def sparse_mean_square_error(sparse_ratings, user_embeddings, movie_embeddings):
+  """
+  Args:
+    sparse_ratings: A SparseTensor rating matrix, of dense_shape [N, M]
+    user_embeddings: A dense Tensor U of shape [N, k] where k is the embedding
+      dimension, such that U_i is the embedding of user i.
+    movie_embeddings: A dense Tensor V of shape [M, k] where k is the embedding
+      dimension, such that V_j is the embedding of movie j.
+  Returns:
+    A scalar Tensor representing the MSE between the true ratings and the
+      model's predictions.
+  """
+  # predictions = tf.gather_nd(
+  #     tf.matmul(user_embeddings, movie_embeddings, transpose_b=True),
+  #     sparse_ratings.indices)
+  predictions = tf.reduce_sum(
+      tf.gather(user_embeddings, sparse_ratings.indices[:, 0]) *
+      tf.gather(movie_embeddings, sparse_ratings.indices[:, 1]),
+      axis=1)
+  loss = tf.losses.mean_squared_error(sparse_ratings.values, predictions)
+  return loss
+
+class CFModel(object):
+  """Simple class that represents a collaborative filtering model"""
+  def __init__(self, embedding_vars, loss, metrics=None):
+    """Initializes a CFModel.
+    Args:
+      embedding_vars: A dictionary of tf.Variables.
+      loss: A float Tensor. The loss to optimize.
+      metrics: optional list of dictionaries of Tensors. The metrics in each
+        dictionary will be plotted in a separate figure during training.
+    """
+    self._embedding_vars = embedding_vars
+    self._loss = loss
+    self._metrics = metrics
+    self._embeddings = {k: None for k in embedding_vars}
+    self._session = None
+
+  @property
+  def embeddings(self):
+    """The embeddings dictionary."""
+    return self._embeddings
+
+  def train(self, num_iterations=100, learning_rate=1.0, 
+            optimizer=tf.train.GradientDescentOptimizer):
+    """Trains the model.
+    Args:
+      iterations: number of iterations to run.
+      learning_rate: optimizer learning rate.
+      optimizer: the optimizer to use. Default to GradientDescentOptimizer.
+    Returns:
+      The metrics dictionary evaluated at the last iteration.
+    """
+    with self._loss.graph.as_default():
+      opt = optimizer(learning_rate)
+      train_op = opt.minimize(self._loss)
+      local_init_op = tf.group(
+          tf.variables_initializer(opt.variables()),
+          tf.local_variables_initializer())
+      if self._session is None:
+        self._session = tf.Session()
+        with self._session.as_default():
+          self._session.run(tf.global_variables_initializer())
+          self._session.run(tf.tables_initializer())
+          tf.train.start_queue_runners()
+
+    with self._session.as_default():
+      local_init_op.run()
+      iterations = []
+      metrics = self._metrics or ({},)
+      metrics_vals = [collections.defaultdict(list) for _ in self._metrics]
+
+      # Train and append results.
+      for i in range(num_iterations + 1):
+        _, results = self._session.run((train_op, metrics))
+        if (i % 10 == 0) or i == num_iterations:
+          print("\r iteration %d: " % i + ", ".join(
+                ["%s=%f" % (k, v) for r in results for k, v in r.items()]),
+                end='')
+          iterations.append(i)
+          for metric_val, result in zip(metrics_vals, results):
+            for k, v in result.items():
+              metric_val[k].append(v)
+
+      for k, v in self._embedding_vars.items():
+        self._embeddings[k] = v.eval()
+
+      return results
+
+def build_model(train_dataset, eval_dataset, embedding_dim=3, init_stddev=1.):
+  """
+  Args:
+    ratings: a DataFrame of the ratings
+    embedding_dim: the dimension of the embedding vectors.
+    init_stddev: float, the standard deviation of the random initial embeddings.
+  Returns:
+    model: a CFModel.
+  """
+
+  # SparseTensor representation of the train and test datasets.
+  A_train = build_rating_sparse_tensor(train_dataset)
+  A_test = build_rating_sparse_tensor(eval_dataset)
+  # Initialize the embeddings using a normal distribution.
+  U = tf.Variable(tf.random_normal(
+      [A_train.dense_shape[0], embedding_dim], stddev=init_stddev))
+  V = tf.Variable(tf.random_normal(
+      [A_train.dense_shape[1], embedding_dim], stddev=init_stddev))
+  train_loss = sparse_mean_square_error(A_train, U, V)
+  test_loss = sparse_mean_square_error(A_test, U, V)
+  metrics = {
+      'train_error': train_loss,
+      'test_error': test_loss
+  }
+  embeddings = {
+      "user_id": U,
+      "movie_id": V
+  }
+  return CFModel(embeddings, train_loss, [metrics])
+
+def run(work_dir, max_iterations):
 
   train_dataset = load(os.path.join(args.work_dir, 'train-dataset'))
   eval_dataset = load(os.path.join(args.work_dir, 'eval-dataset'))
 
-  print(train_dataset)
+  # Build the CF model and train it.
+  model = build_model(train_dataset, eval_dataset, embedding_dim=30, init_stddev=0.5)
+  model.train(num_iterations=num_iterations, learning_rate=10.)
+
 
 if __name__ == '__main__':
   """Main function called by AI Platform."""
@@ -33,13 +169,13 @@ if __name__ == '__main__':
            'This can be a Google Cloud Storage path.')
 
   parser.add_argument(
-      '--train-max-epoches',
+      '--max_iterations',
       type=int,
       default=1000,
-      help='Number of epoches to train the model')
+      help='Number of iterations to train the model')
 
   args = parser.parse_args()
 
   run(
     args.work_dir,
-    args.train_max_epoches)
+    args.max_iterations)
